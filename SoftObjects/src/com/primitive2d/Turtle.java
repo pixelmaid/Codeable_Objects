dd40dd73306b727c6e843d06c3dfadf84e059891
pixelmaid <jacobsj@media.mit.edu>
2012-07-03 11:04:21 -0400

    corrected turtle movement

---
+++ SoftObjects/src/com/primitive2d/Turtle.java
@@ -0,0 +1,20 @@
+package com.primitive2d;
+import com.datatype.Point;
+
+
+interface Turtle {
+
+	public void left(double angle);
+	
+	public void right(double angle);
+	
+	public void forward(double dist);
+	
+	public void back(double dist);
+	
+	public void penUp();
+	
+	public void penDown();
+	
+	
+}
